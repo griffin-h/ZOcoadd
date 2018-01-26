8ac795b32ea7b7a1f439ad37f4dfcc9cd5927641
Griffin Hosseinzadeh <g.hosseinzadeh90@gmail.com>
2018-01-26 15:42:41 -0500

    add command line options for combining fits files

--- coadd.py
+++ coadd.py
@@ -1,4 +1,6 @@
 import numpy as np
+from astropy.io import fits
+from argparse import ArgumentParser
 
 def stack_images(images, psfs, variances, flux_zps):
     '''Coadd a list of images using the Zackay & Ofek (2017) algorithm'''
@@ -20,3 +22,24 @@ def normalize_stacked_image(stacked, psfs, variances, flux_zps, flux_units=False
     else:
         stacked_norm = stacked / norm_factor**0.5
     return stacked_norm
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Coadd a list of images using the Zackay & Ofek (2017) algorithm')
+    parser.add_argument('images', nargs='+', help='filenames to stack')
+    parser.add_argument('--psf', nargs='+', help='filenames of PSF images')
+    parser.add_argument('--zp-keyword', help='header keyword containing zero point')
+    parser.add_argument('--sky-keyword', help='header keyword containing standard deviation of sky')
+    parser.add_argument('--flux-units', action='store_true', help='return stacked image in flux units, rather than standard deviations')
+    parser.add_argument('--output', help='output filename')
+    args = parser.parse_args()
+    
+    images = [fits.getdata(fn) for fn in args.images]
+    headers = [fits.getheader(fn) for fn in args.images]
+    flux_zps = [hdr[args.zp_keyword] for hdr in headers]
+    variances = [hdr[args.sky_keyword]**2 for hdr in headers]
+    psfs = [fits.getdata(fn) for fn in args.psf]
+    
+    stacked = stack_images(images, psfs, variances, flux_zps)
+    stacked_norm = normalize_stacked_image(stacked, psfs, variances, flux_zps, args.flux_units)
+    
+    fits.writeto(args.output, stacked_norm)
